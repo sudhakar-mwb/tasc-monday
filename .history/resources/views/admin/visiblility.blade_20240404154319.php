c40cb9d185513fb4d9d8912a2b609abcbac29660
jaikrishnaverma-cedcoss <jaikrishnaverma@cedcoss.com>
2024-04-04 19:20:35 +0530

    status group taken form

---
+++ .history/resources/views/admin/visiblility.blade_20240404154319.php
@@ -0,0 +1,117 @@
+@include('includes.header')
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/2.0.3/css/dataTables.dataTables.css" />
+<script src="https://cdn.datatables.net/2.0.3/js/dataTables.js"></script>
+
+<main class="px-3 pt-5 ">
+    @include('admin.headtitle')
+    <div class="d-flex w-100 justify-content-center" style="">
+
+        <div style="max-width:500px;width:100%;">
+            <div class="bg-success text-white">
+                <p class="p-2 m-0"><strong>MANAGE COLUMNS</strong></p>
+            </div>
+            <div class="form_wrapper border border-success p-4">
+                <form id="column_view_form" class="text-start ">
+                    <div class="mb-3" id="form-step-1">
+                        <label for="exampleInputEmail1" class="form-label">Select Board</label>
+                        <select id="input-brand" class="form-select" id="exampleInputEmail1"
+                            aria-label="Default select example" required>
+                            <option value="">-- Select  Brand --</option>
+                            @for ($j = 0; $j < count($boards); $j++)
+                            <option value="{{ $boards[$j] }}">{{ $boards[$j] }}</option>
+                        @endfor
+                        </select>
+                    </div>
+
+                    <div class="hiddenstep" class="" id="form-step-2">
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Onboarding Status</label>
+                            <select id="onboarding_columns" class="js-example-basic-multiple w-100"
+                                name="onboarding_coulumns[]" multiple="multiple" style="max-width:500px" >
+
+                                @for ($j = 0; $j < count($boards); $j++)
+                                    <option value="{{ $boards[$j] }}">{{ $boards[$j] }}</option>
+                                @endfor
+                            </select>
+                            {{-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> --}}
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Candidate Information</label>
+                            <select id="candidate_columns" class="js-example-basic-multiple w-100"
+                                name="candidate_columns[]" multiple="multiple" style="max-width:500px" >
+
+                                @for ($j = 0; $j < count($boards); $j++)
+                                    <option value="{{ $boards[$j] }}">{{ $boards[$j] }}</option>
+                                @endfor
+                            </select>
+                        </div>
+                        <div class="mb-3" id="icon_inputs-wrapper">
+                            <label for="exampleInputPassword1" class="form-label">Provide candidate column details : </label>
+                          
+                            <a href="https://icons.getbootstrap.com/" target="_blank">Go to icons librery&nbsp;<i class="bi bi-box-arrow-up-right"></i></a>   <ul id="icon_inputs">
+                            </ul>
+                        </div>
+                      
+                        <hr class="mt-4">
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Onboarding Updates</label>
+                        <select id="onboarding-updates-option" class="form-select" aria-label="Default select example" required>
+                    </select>
+                    </div>
+                    </div>
+
+                    <div class="d-grid gap-2 mt-5"> <button id="columns_details_submit" type="submit"
+                            class="btn btn-success ">Submit</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+
+    </div>
+
+
+    <style>
+        form .hiddenstep {
+            display: none
+        }
+
+        .select2-selection.select2-selection--multiple {
+            min-height: 100px;
+            width: 100%
+        }
+
+        .select2 {
+            width: 100% !important;
+            max-width: 500px;
+        }
+
+        .form_wrapper {
+            border-radius: 0px 0px 5px 5px;
+        }
+    </style>
+    <script src="{{ asset('script/columns-form.js') }}"></script>
+    <script>
+        $(document).ready(function() {
+            $('.js-example-basic-multiple').select2();
+        });
+        $('#columnsTable').DataTable();
+    </script>
+</main>
+<div class="full-loader d-flex align-items-center justify-content-center" >
+<img src="{{ asset('asset/loading4.gif') }}" alt="" style="border: 5px">
+</div>
+<style>
+  .full-loader{
+    position: fixed; 
+    background-color:rgba(0, 94, 255, 0.297);
+    height:100vh;
+    width:100vw;
+    top:0;
+    left:0;
+    border: 2px solid rgb(0, 94, 255);
+  }
+</style>
+@include('includes.footer')
---
+++ .history/resources/views/auth/signup.blade_20240404131742.php
@@ -0,0 +1,64 @@
+@include('auth.header')
+
+
+<main class="px-3 pt-5">
+    @include('admin.headtitle')
+
+    @if($status!="")
+<div class="d-flex justify-content-center">
+    <div class="alert alert-{{$status}}" style="max-width:400px">    {{$msg}}</div>
+</div>
+    @endif
+    
+
+    <form class="form-auth" id="registration-custom-form" action="{{route('monday.post.signup')}}" method="POST">
+        {{-- @csrf --}}
+        <input type="text" placeholder="Name*" name="name" value="{{ old('name') }}">
+        @error('name')<small class="text-danger text-start ms-2">{{ $message }}</small>@enderror
+        <input type="text" placeholder="Company name*" name="company_name" value="{{ old('company_name') }}">
+        @error('company_name')<small class="text-danger text-start ms-2">{{ $message }}</small>@enderror
+        <input type="text" placeholder="Phone*" name="phone" value="{{ old('phone') }}">
+        @error('phone')<small class="text-danger text-start ms-2">{{ $message }}</small>@enderror
+        <input type="text" placeholder="Email*" name="email" value="{{ old('email') }}">
+        @error('email')<small class="text-danger text-start ms-2">{{ $message }}</small>@enderror
+        <input type="text" placeholder="Password*" name="password" value="{{ old('password') }}">
+        @error('password')<small class="text-danger text-start ms-2">{{ $message }}</small>@enderror
+
+        <div class="w-100 d-flex justify-content-center">
+            <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+            {{-- secret key:  6LchU6gpAAAAAAgx4pH2xz9R0VYoDn-c-T4RQri7 --}}
+            <div class="g-recaptcha" data-sitekey="6LchU6gpAAAAAPFn3HHkKDNlOJ_rn7CdX7y6b2XR"></div>
+
+        </div>
+        <button class="btn btn-to-link btn-secondary mt-4 btn-gradiant  d-flex align-items-center" type="submit"
+            {{-- type="button" --}}>
+            <span>
+                {{-- <a href="thanks" class="text-decoration-none text-light"> --}}
+                Request Access
+                {{-- </a> --}}
+
+            </span>
+
+            <span class="icon-btn_track" style="margin-left:10px">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                    <path
+                        d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
+                </svg>
+            </span>
+        </button>
+    </form>
+
+
+    <a href="login">Already have an Account?</a>
+</main>
+{{-- <script>
+    var form = document.getElementById('registration-custom-form');
+    form.addEventListener("submit", function(event) {
+        if (grecaptcha.getResponse() === '') {
+            event.preventDefault();
+            alert('Please check the recaptcha');
+        }
+    }, false);
+</script> --}}
+@include('includes.footer')
