1e4de4860ca36ae1b300c25dbd58b3cd496b27f6
jaikrishnaverma-cedcoss <jaikrishnaverma@cedcoss.com>
2024-05-01 16:47:30 +0530

    dfs

--- resources/views/admin/user_details.blade.php
+++ resources/views/admin/user_details.blade.php
@@ -301,8 +301,8 @@ if ($joiningDate !== null && validText($joiningDate)) {
                                                     viewBox="0 0 16 16">
                                                     <path fill-rule="evenodd"
                                                         d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
-                                                </svg> {{ $status }} |
-                                                {{ isset($valued['changed_at']) && $valued !== null ? dateFormater($valued['changed_at']) : 'Pending' }}
+                                                </svg> {{ $status }} 
+                                                {{ isset($valued['changed_at']) && $valued !== null ? '| '.dateFormater($valued['changed_at']) : '' }}
                                             </span>
                                         @endif
                                     </div>
