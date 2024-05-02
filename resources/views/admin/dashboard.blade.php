a272de8dda34938991e6c6603d778954c60f202f
jaikrishnaverma-cedcoss <jaikrishnaverma@cedcoss.com>
2024-05-02 16:40:29 +0530

    font correction

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -226,7 +226,7 @@ $settings = Session::get('settings');
 /* Extra large devices (large desktops, 1200px and up) */
 @media (min-width: 1200px) {
   .container{
-    max-width: 90% !important;
+    max-width: 1200 !important;
  }
   p.card-desc{
     font-size: 1rem !important;
