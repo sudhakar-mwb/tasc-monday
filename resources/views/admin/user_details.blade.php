88f92cc10ef3f4c6c0f78aa1ee60f78d5c507b38
shivshaktisingh-cedcommerce <shivshaktisingh@cedcommerce.com>
2024-06-04 18:49:10 +0530

    fixes

--- resources/views/admin/user_details.blade.php
+++ resources/views/admin/user_details.blade.php
@@ -276,7 +276,7 @@ if ($joiningDate !== null && validText($joiningDate)) {
                                                     d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
                                             </svg>
                                             {{-- {{ $joiningDate }} --}}
-                                            Initiated | {{ $joiningDate }}</span>
+                                            Awaiting Action | {{ $joiningDate }}</span>
                                         @if ($status != null&&isset($valued['changed_at']))
                                             <span class="text-secondary">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
