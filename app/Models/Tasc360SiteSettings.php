7b421f31ebba8282ec63452aa79ff34878c28b70
sudhakarpandey <sudhakarpandey@makewebbetter.com>
2024-06-28 20:28:36 +0530

    common reset password

--- app/Models/Tasc360SiteSettings.php
+++ app/Models/Tasc360SiteSettings.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Tasc360SiteSettings extends Model
 {
     use HasFactory;
+
+    protected $table = 'tasc360_site_settings';
+    
+    protected $fillable = ['id', 'ui_settings', 'logo_name', 'logo_location','status' ,'updated_at', 'created_at'];
 }
